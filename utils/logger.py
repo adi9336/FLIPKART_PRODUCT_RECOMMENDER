13dd5b08f2dc2720cb4eac24cc3fb623711a76d6
Aditya Gupta <148680267+adi9336@users.noreply.github.com>
2026-07-20 02:58:30 +0530

    Initial project commit

---
+++ utils/logger.py
@@ -0,0 +1,20 @@
+import logging 
+import os
+from datetime import datetime
+
+LOG_DIR="logs"
+
+os.makedirs(LOG_DIR,exist_ok=True)
+
+LOG_FILE = os.path.join(LOG_DIR,f"log_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log")
+
+logging.basicConfig(
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+def get_logger(name:str):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    return logger
